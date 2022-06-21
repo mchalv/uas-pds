fd17806a6eb1ec9cb8f0a0f2357f93f47c186e0f
Gregorts <c14190135@john.petra.ac.id>
2022-06-21 23:01:38 +0700

    tinggal filter

--- Laptop_Catalog.php
+++ Laptop_Catalog.php
@@ -10,13 +10,43 @@
     $brand = $client->uas_pds->brand;
     $cursor_brand = $brand->find();
 
-    $brand_laptop = "";
+    $rating = $client->uas_pds->rating;
+    $cursor_rating = $rating->find();
+
     $count = 0;
+
+    $brand_laptop = "";
     $array_brand = array("");
 
     foreach ($cursor_brand as $value2) {
         array_push($array_brand, $value2->brand);
     }
+
+    $array_jumlah_rating = array();
+
+    $array_jumlah_perating = array();
+
+    for($i = 0; $i < 896; $i++){
+        array_push($array_jumlah_rating, 0);
+        array_push($array_jumlah_perating, 0);
+    }
+
+    foreach ($cursor_rating as $value3) {
+        $id_laptop_rating = $value3->id_laptop;
+        
+        $array_jumlah_rating[$id_laptop_rating - 1] += $value3->rating;
+        $array_jumlah_perating[$id_laptop_rating - 1] += 1;
+    }
+
+    // $array2d = array(array(1, 2, 3, 4), array(5, 5, 5, 5));
+    // print_r($array2d);
+?>
+
+<?php
+    // Include config file
+    require_once "../config/config_sql.php";
+
+    
 ?>
 
 <?php
@@ -46,10 +76,12 @@
             <form action="" method="post">
                 <select name="filter" class="bg-secondary">
                     <option value=pilihan1>High spec and low price</option>
-                    <option value=pilihan1>Best Selling Brand</option>
-                    <option value=pilihan1>High rate and low price</option>
+                    <option value=pilihan2>Best Selling Brand</option>
+                    <option value=pilihan3>High rate and low price</option>
                 </select>
-                <button class="btn btn-outline-success" type="submit" name="filter" value="Filter">Filter</button>
+                <button class="btn btn-outline-success" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Filter
+                </button>
             </form>
             <div class="dropstart">
                 <button type="button" class="btn btn-secondary dropdown-toggle text-black" data-bs-toggle="dropdown" aria-expanded="false">
@@ -65,10 +97,8 @@
 
     <?php foreach ($cursor_laptop as $id => $value) { ?>
         <?php if ($count == 0) { ?>
-            <?php echo '<div class="card-group">'; $count++; ?>
-        <?php } elseif ($count == 4) { ?>
-            <?php echo '</div>'; $count = 0; ?>
-        <?php } else { ?>
+            <div class="card-group">
+        <?php } ?> 
             <div class="card text-white bg-secondary border-dark">
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $value->tipe_laptop ?></h5>
@@ -77,20 +107,34 @@
                             <?php $brand_laptop = $array_brand[$j]; ?>
                         <?php } ?>
                     <?php } ?>
+                    <p class="card-text"><?php echo 'ID : '.$value->id ?></p>
                     <p class="card-text"><?php echo 'Brand : '.$brand_laptop ?></p>
                     <p class="card-text"><?php echo 'Processor : '.$value->processor_name ?></p>
                     <p class="card-text"><?php echo 'Processor Gen : '.$value->processor_gen ?></p>
                     <p class="card-text"><?php echo 'RAM : '.$value->ram." GB" ?></p>
                     <p class="card-text"><?php echo 'RAM Type : '.$value->ram_type ?></p>
-                    <?php if ($value->ssd == "0") { ?>
-                        <p class="card-text"><?php echo 'HDD : '.$value->hdd." GB" ?></p>
-                    <?php } elseif ($value->hdd == "0") { ?>
-                        <p class="card-text"><?php echo 'SSD : '.$value->ssd." GB" ?></p>
-                    <?php } ?>
+                    <p class="card-text"><?php echo 'HDD | SSD : '.$value->hdd." GB | ".$value->ssd." GB" ?></p>
+                    <p class="card-text"><?php echo 'OS : '.$value->os ?></p>
+                    <p class="card-text"><?php echo 'OS Bit : '.$value->os_bit ?></p>
+                    <p class="card-text"><?php echo 'Warranty : '.$value->warranty. " Year" ?></p>
+                    <p class="card-text"><?php echo 'Ms Office : '.$value->ms_office ?></p>
+                    <p class="card-text"><?php echo 'Price : '.$value->price ?></p>
+                    <?php 
+                        if($array_jumlah_perating[$value->id - 1] == 0){
+                            $rata2_rating = 0;
+                        }else{
+                            $rata2_rating = $array_jumlah_rating[$value->id - 1] / $array_jumlah_perating[$value->id - 1];
+                        }
+                    ?>
+                    <p class="card-text"><?php echo 'Rating : '.$rata2_rating." dari jumlah yang merating : ". $array_jumlah_perating[$value->id - 1] ?></p>
                 </div>
             </div>
-        <?php $count++; } ?>
-    <?php } ?>
+            <?php $count++; ?>
+            <?php if ($count == 3) { ?>
+                <?php echo '</div>'; $count = 0; ?>
+            <?php } ?>
+        <?php } ?>
+        
 
     <!-- <div class="card-group">
         <div class="card text-white bg-secondary border-dark">
@@ -127,6 +171,23 @@
             </div>
         </div>
     </div> -->
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
